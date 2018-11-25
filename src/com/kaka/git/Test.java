602864e48ecd3469e3fa705c0986420691848113
gonglu <g514331301@163.com>
2018-11-25 23:45:14 +0800

    modify test ,add dev2

--- src/com/kaka/git/Test.java
+++ src/com/kaka/git/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("git");
 		System.out.println("gonglu_dev_git1");
+		System.out.println("gonglu_dev_git2");
 	}
 
 }
